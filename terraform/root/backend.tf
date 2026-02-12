2c657d8aeb769de91dbd2e8a736d6aecb919e080
john1234567987 <anigbojohn9@gmail.com>
2026-02-12 11:07:38 +0100

    terraform

--- terraform/root/backend.tf
+++ terraform/root/backend.tf
@@ -1,4 +1,9 @@
-
-terraform { 
-        
-    }
+terraform {
+  backend "s3" {
+    bucket         = "todo-list-1987"
+    key            = "state/dev/terraform.tfstate"
+    region         = "eu-west-2"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
+}
