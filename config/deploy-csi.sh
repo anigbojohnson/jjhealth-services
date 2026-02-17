aadf15dcd7b02f394b99311d63bfc5eb1ef9a96c
john1234567987 <anigbojohn9@gmail.com>
2026-02-17 14:43:01 +0100

    Stop tracking Terraform state and local files

--- config/deploy-csi.sh
+++ config/deploy-csi.sh
@@ -13,62 +13,12 @@ BACKEND_SA_NAME="laravel-sa"
 BACKEND_ROLE_NAME="jjhealth-backend-secrets-role-2"
 BACKEND_POLICY_NAME="jjhealth-backend-secrets-policy"
 
-### ===============================
-### 1. UPDATE KUBECONFIG
-### ===============================
-
-echo "🔁 Updating kubeconfig..."
-aws eks update-kubeconfig \
-  --region ${AWS_REGION} \
-  --name ${CLUSTER_NAME}
-
-kubectl get nodes
-
-### ===============================
-### 2. CREATE IAM ROLE + SERVICE ACCOUNT (IRSA)
-### ===============================
-
-eksctl utils associate-iam-oidc-provider \
-  --cluster "${CLUSTER_NAME}" \
-  --region "${AWS_REGION}" \
-  --approve
-
-echo "🔐 Creating IAM ServiceAccount: ${BACKEND_SA_NAME} in namespace ${NAMESPACE}"
-
-eksctl delete iamserviceaccount \
-  --cluster "${CLUSTER_NAME}" \
-  --namespace "${NAMESPACE}" \
-  --name ${BACKEND_SA_NAME}
-
-
-eksctl create iamserviceaccount \
-  --cluster "${CLUSTER_NAME}" \
-  --namespace "${NAMESPACE}" \
-  --name "${BACKEND_SA_NAME}" \
-  --role-name "${BACKEND_ROLE_NAME}" \
-  --attach-policy-arn "arn:aws:iam::${AWS_ACCOUNT_ID}:policy/${BACKEND_POLICY_NAME}" \
-  --override-existing-serviceaccounts \
-  --approve
 
 
 
-### ===============================
-### 3. INSTALL / UPDATE CSI DRIVER
-### ===============================
-
-echo "📦 Installing Secrets Store CSI Driver..."
-
-helm repo add secrets-store-csi-driver https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts
+helm repo add external-secrets https://charts.external-secrets.io
 helm repo update
 
-helm upgrade --install csi-secrets-store \
-  secrets-store-csi-driver/secrets-store-csi-driver \
-  --namespace kube-system
-
-### ===============================
-### 4. INSTALL AWS CSI PROVIDER
-### ===============================
-
-kubectl apply -f https://raw.githubusercontent.com/aws/secrets-store-csi-driver-provider-aws/main/deployment/aws-provider-installer.yaml
-
-echo "✅ CSI Driver setup completed!"
+helm upgrade --install external-secrets external-secrets/external-secrets \
+  --namespace external-secrets \
+  --create-namespace
