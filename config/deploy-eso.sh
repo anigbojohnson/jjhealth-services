fddd7ae4fead48944dc03556c75e7ff406a1c136
john1234567987 <anigbojohn9@gmail.com>
2026-02-19 11:02:04 +0100

    jesus is lord

---
+++ config/deploy-eso.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+
+
+helm repo add external-secrets https://charts.external-secrets.io
+helm repo update
+
+helm upgrade --install external-secrets external-secrets/external-secrets \
+  --namespace external-secrets \
+  --create-namespace \
+  --set serviceAccount.create=true \
+  --set serviceAccount.annotations."eks\.amazonaws\.com/role-arn"="arn:aws:iam::869868778582:role/jjhealth-backend-secrets-role"
+
