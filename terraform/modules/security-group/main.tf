8ea38c0a728498cb563de0a2eb9f4131ff9e2115
Johnson <anigbojohnsona@gmail.com>
2026-02-27 10:31:13 -0800

    test

--- terraform/modules/security-group/main.tf
+++ terraform/modules/security-group/main.tf
@@ -11,6 +11,13 @@ resource "aws_security_group" "alb_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "HTTPS from internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     description = "all outbound"
