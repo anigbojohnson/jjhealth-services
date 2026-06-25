c7e6741ba4fbd914bc1905b800ea0729121a2be3
Anigbo Johnson <anigbojohnsona@gmail.com>
2026-06-25 12:04:02 +0100

    testing vault

--- terraform/vault/ec2.tf
+++ terraform/vault/ec2.tf
@@ -170,7 +170,8 @@ resource "aws_iam_role_policy" "vault-aws-root-recovery-token-secrets-engine" {
         "secretsmanager:PutSecretValue",
         "secretsmanager:TagResource",
         "secretsmanager:GetResourcePolicy",
-        "secretsmanager:DeleteSecret"
+        "secretsmanager:DeleteSecret",
+        "secretsmanager:RestoreSecret"
       ],
 
       "Resource":[ "arn:aws:secretsmanager:eu-west-2:869868778582:secret:jjhealth-services/vault/root-token*",
