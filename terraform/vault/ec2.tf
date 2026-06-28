21ebafdf6dc5e5f45d1d777c82d5f9dea4082fd1
Anigbo Johnson <anigbojohnsona@gmail.com>
2026-06-28 15:05:40 +0100

    testing vault

--- terraform/vault/ec2.tf
+++ terraform/vault/ec2.tf
@@ -131,7 +131,6 @@ resource "aws_instance" "private" {
   tags = { Name = "private-ec2" }
 }
 
-
 resource "aws_instance" "public" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
