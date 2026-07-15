425200df1a80d5202dc3a816f50cd597383fa832
Anigbo Johnson <anigbojohnsona@gmail.com>
2026-07-15 14:57:28 +0100

    vault testing

--- terraform/vault/secret-manager.tf
+++ terraform/vault/secret-manager.tf
@@ -1,7 +1,7 @@
 locals {
   vault_secrets = {
-    root_token    = "jjhealth-services/vault/root-token-26"
-    recovery_keys = "jjhealth-services/vault/recovery-keys-26"
+    root_token    = "jjhealth-services/vault/root-token-27"
+    recovery_keys = "jjhealth-services/vault/recovery-keys-27"
   }
 }
 
