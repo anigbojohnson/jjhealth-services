b48ba58801c618c32135444115610ef1d96bdec4
Anigbo Johnson <anigbojohnsona@gmail.com>
2026-07-16 21:32:11 +0100

    vault testing

--- terraform/vault/ec2.tf
+++ terraform/vault/ec2.tf
@@ -144,6 +144,7 @@ resource "aws_iam_instance_profile" "public" {
   role = aws_iam_role.public.name
 }
 
+
 resource "aws_instance" "private" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
