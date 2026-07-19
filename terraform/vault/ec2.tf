ceff27393c4147b00bdef57f93ee922ad30c4611
Anigbo Johnson <anigbojohnsona@gmail.com>
2026-07-19 11:17:15 +0100

    vault testing

--- terraform/vault/ec2.tf
+++ terraform/vault/ec2.tf
@@ -31,7 +31,7 @@ locals {
     tls_secret = var.vault_key_certificate_secret
     ca_secret  = var.ca_certificate_secret
     aws_region = var.region
-    tls_dir    = "/etc/vault.d/tls"
+    tls_dir    = "/etc/tls/bootstrap"
   })
 }
 
