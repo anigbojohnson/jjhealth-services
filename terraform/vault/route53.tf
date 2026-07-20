15d4c2ab87bd3274b6f24751e391cb88d2091a7f
Anigbo Johnson <anigbojohnsona@gmail.com>
2026-07-20 15:38:16 +0100

    vault testing

--- terraform/vault/route53.tf
+++ terraform/vault/route53.tf
@@ -8,7 +8,7 @@ resource "aws_route53_zone" "private" {
 
 resource "aws_route53_record" "vault" {
   zone_id = aws_route53_zone.private.zone_id
-  name    = "backed"
+  name    = "backend"
   type    = "A"
   ttl     = 300
 
