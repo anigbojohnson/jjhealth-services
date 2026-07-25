d6d7162a18d6c7718b91beac8e4703265f1b2af6
Anigbo Johnson <anigbojohnsona@gmail.com>
2026-07-25 12:28:50 +0100

    vault testing

--- terraform/vault/ec2.tf
+++ terraform/vault/ec2.tf
@@ -299,6 +299,7 @@ resource "aws_iam_role_policy" "vault_aws_secrets_engine" {
         "iam:AttachUserPolicy",
         "iam:PutUserPolicy",
         "iam:DeleteUserPolicy",
+        "iam:GetRole",
         "sts:AssumeRole"
       ]
       Resource = "*"
